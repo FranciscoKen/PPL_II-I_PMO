61097165e05a6924ff711bf40ed689ef754346d3
Franciscoken <franciscokenandi@gmail.com>
2018-03-09 11:25:30 +0700

    initialize android project

---
+++ android/app/src/main/java/com/example/ppl/pmotrainingapps/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ppl.pmotrainingapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
