5746f05f7792bae07af6727cd436d3a47d2e37e1
Dicky Novanto <13515134@std.stei.itb.ac.id>
2018-03-17 11:31:01 +0700

    added empty activity of login page

---
+++ android/app/src/main/java/com/example/ppl/pmotrainingapps/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.ppl.pmotrainingapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
