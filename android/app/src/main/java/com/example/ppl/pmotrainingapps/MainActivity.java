79007190ef669fc5dd93be359bb5bc68538fe352
Dicky Novanto <13515134@std.stei.itb.ac.id>
2018-03-18 22:59:50 +0700

    add code to show name and NIP after inputting

--- android/app/src/main/java/com/example/ppl/pmotrainingapps/MainActivity.java
+++ android/app/src/main/java/com/example/ppl/pmotrainingapps/MainActivity.java
@@ -3,15 +3,32 @@ package com.example.ppl.pmotrainingapps;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String statusLogin = intent.getStringExtra("statusLogin");
+        Log.d("isi","statusLogin: "+statusLogin);
+        if(statusLogin == null){
+            Intent loginScreen = new Intent(getBaseContext(), LoginActivity.class);
+            startActivity(loginScreen);
+            finish();
+        }else{
+            String nama = intent.getStringExtra("nama");
+            String NIP = intent.getStringExtra("NIP");
+            TextView textView = (TextView) findViewById(R.id.textMain);
+            textView.setText("hai "+nama+" berNIP: "+NIP);
+            Log.d("terlihat", "tertulisNama: "+nama+" berNIP: "+NIP);
+        }
 
-//        Intent loginScreen = new Intent(getBaseContext(), LoginActivity.class);
-//        this.startActivity(loginScreen);
+
+
+
     }
 }
