71c7e934dcbcb632c02b06ad5f9469283d121438
X550ZE\David <davidtheoxcalibur@gmail.com>
2018-03-24 09:29:06 +0700

    add comment text box

---
+++ android/app/src/main/java/ppl/pmotrainingapps/content/Comment.java
@@ -0,0 +1,24 @@
+package ppl.pmotrainingapps.content;
+
+
+public class Comment {
+    /**
+     * Product ID.
+     */
+    public String productId;
+
+    /**
+     * User ID.
+     */
+    public String userId;
+
+    /**
+     * User name.
+     */
+    public String userName;
+
+    /**
+     * Comment text.
+     */
+    public String text;
+}
