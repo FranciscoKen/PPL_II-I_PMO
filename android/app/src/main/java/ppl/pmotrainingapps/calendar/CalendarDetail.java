e91fcd0a06c8c76c12583728d71e1630c9c76be5
ayamberkakienam <zackhardyz@gmail.com>
2018-04-09 08:24:37 +0700

    Create AsyncTask class inside CalendarDetail

--- android/app/src/main/java/ppl/pmotrainingapps/calendar/CalendarDetail.java
+++ android/app/src/main/java/ppl/pmotrainingapps/calendar/CalendarDetail.java
@@ -1,5 +1,6 @@
 package ppl.pmotrainingapps.calendar;
 
+import android.os.AsyncTask;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,7 +9,18 @@ import android.widget.TextView;
 
 import org.json.JSONObject;
 import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 import ppl.pmotrainingapps.Home.AdapterPengumuman;
@@ -41,4 +53,50 @@ public class CalendarDetail extends AppCompatActivity {
     }
 
     private void prepareKegiatan() {}
+
+    private static class KegiatanTask extends AsyncTask<Void, Void, Void> {
+
+        private WeakReference<CalendarDetail> activityReference;
+
+        KegiatanTask(CalendarDetail context) {
+            activityReference = new WeakReference<>(context);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            StringBuilder result = new StringBuilder();
+            try {
+                URL url = new URL("http://pplk2a.if.itb.ac.id/ppl/getAllWHAT.php");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                if (connection.getResponseCode() == 200) {
+                    InputStream responseBody = new BufferedInputStream(connection.getInputStream());
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(responseBody));
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    JSONObject jsonObject = (JSONObject) new JSONParser().parse(result.toString());
+                    CalendarDetail.hasilKegiatan = jsonObject;
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            CalendarDetail activity = activityReference.get();
+            // TODO: activity.setKegiatan();
+        }
+    }
 }
