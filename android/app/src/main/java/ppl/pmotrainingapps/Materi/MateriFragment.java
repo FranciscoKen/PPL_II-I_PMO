193eb9b34903a96cd12105eb201e07d347672101
Dicky Novanto <13515134@std.stei.itb.ac.id>
2018-04-14 01:38:04 +0700

    materiFragment.java done

--- android/app/src/main/java/ppl/pmotrainingapps/Materi/MateriFragment.java
+++ android/app/src/main/java/ppl/pmotrainingapps/Materi/MateriFragment.java
@@ -2,33 +2,51 @@ package ppl.pmotrainingapps.Materi;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.NestedScrollingChild;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import ppl.pmotrainingapps.R;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link MateriFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link MateriFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class MateriFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private final static String TAG = "MateriFragment";
+    public static JSONArray materi = null;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private RecyclerView recyclerView;
+    private NestedScrollView nestedScrollView;
+    private AdapterMateri adapterMateri;
+    private List<Materi> materiList;
 
     public MateriFragment() {
         // Required empty public constructor
@@ -45,62 +63,102 @@ public class MateriFragment extends Fragment {
     // TODO: Rename and change types and number of parameters
     public static MateriFragment newInstance(String param1, String param2) {
         MateriFragment fragment = new MateriFragment();
-
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_materi, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_materi, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view_materi);
+        nestedScrollView = (NestedScrollView) view.findViewById(R.id.nestedScrollMateri);
+        recyclerView.setFocusable(false);
+        nestedScrollView.requestFocus();
+        materiList = new ArrayList<>();
+        adapterMateri = new AdapterMateri(getContext(), materiList);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapterMateri);
+        recyclerView.setNestedScrollingEnabled(false);
+
+        prepareMateri();
+
+        return view;
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+    }
+
+    private void prepareMateri() {
+        new MateriTask(this).execute();
+    }
+    public void setMateri() {
+        if(materi != null) {
+            for(int iterator = 0; iterator < materi.size(); iterator++) {
+                String hasilFetch = materi.get(iterator).toString();
+
+                try {
+                    JSONObject json = (JSONObject) new JSONParser().parse(hasilFetch);
+                    int id_materi = Integer.parseInt((String)json.get("id"));
+                    String topik = (String)json.get("topik");
+                    String judul = (String)json.get("judul");
+                    String konten = (String)json.get("konten");
+                    String video = (String)json.get("video");
+                    String pdf = (String)json.get("pdf");
+
+                    Materi materiSekarang = new Materi(id_materi, topik, judul, konten, video, pdf);
+                    materiList.add(materiSekarang);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+            adapterMateri.notifyDataSetChanged();
         }
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
+    private static class MateriTask extends AsyncTask<Void, Void, Void> {
+        private WeakReference<MateriFragment> activityReference;
+        MateriTask(MateriFragment context) {
+            activityReference = new WeakReference<>(context);
+        }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        protected Void doInBackground(Void... urls) {
+            StringBuilder result = new StringBuilder();
+            try {
+                URL url = new URL("http://pplk2a.if.itb.ac.id/ppl/getAllMateri.php");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                if(connection.getResponseCode() == 200) {
+                    InputStream responseBody = new BufferedInputStream(connection.getInputStream());
+
+                    JSONParser jsonParser = new JSONParser();
+                    JSONArray jsonArray = (JSONArray) jsonParser.parse(new InputStreamReader(responseBody, "UTF-8"));
+                    MateriFragment.materi = jsonArray;
+
+
+                }else{
+                    Log.d("test", "connection failed");
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            MateriFragment activity = activityReference.get();
+            activity.setMateri();
+        }
+
     }
 }
