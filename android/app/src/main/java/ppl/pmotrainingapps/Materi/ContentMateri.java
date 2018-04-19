c83fe4356ad53a11c644e27258abcef4e377f6a8
Dicky Novanto <13515134@std.stei.itb.ac.id>
2018-04-19 11:21:56 +0700

    add code so that when item materi clicked, can go to contentMateri

--- android/app/src/main/java/ppl/pmotrainingapps/Materi/ContentMateri.java
+++ android/app/src/main/java/ppl/pmotrainingapps/Materi/ContentMateri.java
@@ -1,14 +1,15 @@
 package ppl.pmotrainingapps.Materi;
 
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
-import android.widget.VideoView;
+import android.os.Bundle;
 
-import ppl.pmotrainingapps.PDF.PDFViewer;
+import ppl.pmotrainingapps.R;
 
-public class ContentMateri extends AppCompatActivity{
-    TextView judulMateri;
-    TextView kontenMateri;
-    VideoView videoMateri;
+public class ContentMateri extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_materi);
+    }
 }
